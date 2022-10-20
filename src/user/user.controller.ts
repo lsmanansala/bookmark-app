f89f9d4ba6e614dc9d1f54078d8bd5430eebe227
Ludwig Manansala <manansalaludwig@gmail.com>
2022-10-20 20:42:32 +0800

    signin and signup functionality, guards

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe() {
+    return 'user info';
+  }
+}
